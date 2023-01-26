eaa60b614e4fa6114894d1b17922c62401f7f4b4
Alberto Mozo <79279132+albertomozo@users.noreply.github.com>
2023-01-26 14:58:13 +0100

    Update inc_conexion_peliculas_remota.php

--- admin/inc_conexion_peliculas_remota.php
+++ admin/inc_conexion_peliculas_remota.php
@@ -7,7 +7,7 @@ $conpel=mysqli_connect($servidor,$bduser,$bdclave,$bdnombre);
 if ($conpel){
     mysqli_set_charset($conpel,"utf8");
 }
-$tmdb_apikey='98fee347b91da83932ea8b9daa0edece';
+$tmdb_apikey='********************';
 $tmdb_ruta_poster = 'https://image.tmdb.org/t/p/w154'; // 
 $tmdb_ruta_poster_500 = 'https://image.tmdb.org/t/p/w500';
 ?>
