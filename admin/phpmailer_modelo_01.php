c7c090ef28942d4ebcc35cc742ade9c874664a6f
Alberto Mozo <79279132+albertomozo@users.noreply.github.com>
2023-01-27 11:14:12 +0100

    Update phpmailer_modelo_01.php

--- admin/phpmailer_modelo_01.php
+++ admin/phpmailer_modelo_01.php
@@ -17,8 +17,8 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'albertomozodesarrollador@gmail.com';                     //SMTP username
-    $mail->Password   = 'llkauovkgcjbrxfz';                               //SMTP password
+    $mail->Username   = 'YOUR MAIL';                     //SMTP username
+    $mail->Password   = 'YOUR SMTP PASSWORD';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
